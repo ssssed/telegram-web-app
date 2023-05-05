626d6ec13eae142e53162536ae1dbc0be0dd7440
Igor Koneshov <igorkoneshov@mail.ru>
2023-05-05 14:06:06 +0400

    Updates

---
+++ assets/index-904fcfdc.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-1df182db.js";const r=()=>window.Telegram.WebApp,i=()=>{var s,n;const e=r();return a.jsxs("div",{children:["Main: ",(n=(s=e==null?void 0:e.initDataUnsafe)==null?void 0:s.user)==null?void 0:n.username]})};export{i as default};
---
+++ assets/index-f6a8252d.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-1df182db.js";const s=()=>t.jsx("div",{children:"Chat"});export{s as default};
