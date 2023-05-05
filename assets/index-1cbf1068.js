d37b7cb2e1a34f4669e77c40f12f5d8fb5be04be
Igor Koneshov <igorkoneshov@mail.ru>
2023-05-05 18:13:02 +0400

    Updates

---
+++ assets/index-1cbf1068.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-fe0eec44.js";const s=()=>t.jsx("div",{children:"Chat"});export{s as default};
--- assets/index-f6a8252d.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-1df182db.js";const s=()=>t.jsx("div",{children:"Chat"});export{s as default};
